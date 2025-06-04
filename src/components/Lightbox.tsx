1f18ff24b92ca32a1d3934794c372869a1f91f6f
Austin Schey <aschey13@gmail.com>
2025-06-03 22:45:47 -0700

    add image captions

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -1,27 +1,30 @@
 import Lightbox from "yet-another-react-lightbox";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import Captions from "yet-another-react-lightbox/plugins/captions";
 import { useStore } from '@nanostores/preact';
 import { galleryIndex } from '../scripts/gallery.ts';
 import { useRef } from "preact/hooks";
 
 import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/captions.css";
 
 interface Props {
-  images: ImageMetadata[],
+    images: ImageMetadata[],
 }
 
 const Gallery = ({ images }: Props) => {
-  const $galleryIndex = useStore(galleryIndex);
-  const zoomRef = useRef(null);
+    const $galleryIndex = useStore(galleryIndex);
+    const zoomRef = useRef(null);
 
-  return <Lightbox
-    index={$galleryIndex}
-    plugins={[Zoom]}
-    zoom={{ ref: zoomRef }}
-    slides={images}
-    open={$galleryIndex >= 0}
-    close={() => galleryIndex.set(-1)}
-  />
+    return <Lightbox
+        index={$galleryIndex}
+        plugins={[Zoom, Captions]}
+        zoom={{ ref: zoomRef }}
+        captions={{ descriptionTextAlign: 'center', descriptionMaxLines: 10 }}
+        slides={images}
+        open={$galleryIndex >= 0}
+        close={() => galleryIndex.set(-1)}
+    />
 
 }
 
