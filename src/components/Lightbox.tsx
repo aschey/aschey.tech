43e9ca30116d96832ce3349c67ab5083a60bbad2
Austin Schey <aschey13@gmail.com>
2025-06-08 10:29:25 -0700

    fmt

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -1,39 +1,48 @@
-import Lightbox, { type CaptionsRef, type ZoomRef } from "yet-another-react-lightbox";
+import Lightbox, {
+  type CaptionsRef,
+  type ZoomRef,
+} from "yet-another-react-lightbox";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import Captions from "yet-another-react-lightbox/plugins/captions";
-import { useStore } from '@nanostores/preact';
-import { galleryIndex } from '../scripts/gallery.ts';
+import { useStore } from "@nanostores/preact";
+import { galleryIndex } from "../scripts/gallery.ts";
 import { useRef, type MutableRef } from "preact/hooks";
 
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/captions.css";
 
 interface Props {
-    images: ImageMetadata[],
+  images: ImageMetadata[];
 }
 
 const Gallery = ({ images }: Props) => {
-    const $galleryIndex = useStore(galleryIndex);
-    const zoomRef: MutableRef<ZoomRef | null> = useRef(null);
-    const captionsRef: MutableRef<CaptionsRef | null> = useRef(null);
+  const $galleryIndex = useStore(galleryIndex);
+  const zoomRef: MutableRef<ZoomRef | null> = useRef(null);
+  const captionsRef: MutableRef<CaptionsRef | null> = useRef(null);
 
-    return <Lightbox
-        index={$galleryIndex}
-        plugins={[Zoom, Captions]}
-        zoom={{ ref: zoomRef }}
-        captions={{ descriptionTextAlign: 'center', descriptionMaxLines: 10, ref: captionsRef, showToggle: true }}
-        slides={images}
-        open={$galleryIndex >= 0}
-        close={() => galleryIndex.set(-1)}
-        on={{
-            click: () => {
-                (captionsRef.current?.visible
-                    ? captionsRef.current?.hide
-                    : captionsRef.current?.show)?.();
-            }
-        }}
+  return (
+    <Lightbox
+      index={$galleryIndex}
+      plugins={[Zoom, Captions]}
+      zoom={{ ref: zoomRef }}
+      captions={{
+        descriptionTextAlign: "center",
+        descriptionMaxLines: 10,
+        ref: captionsRef,
+        showToggle: true,
+      }}
+      slides={images}
+      open={$galleryIndex >= 0}
+      close={() => galleryIndex.set(-1)}
+      on={{
+        click: () => {
+          (captionsRef.current?.visible
+            ? captionsRef.current?.hide
+            : captionsRef.current?.show)?.();
+        },
+      }}
     />
-
-}
+  );
+};
 
 export default Gallery;
--- src/scripts/contour.ts
+++ src/scripts/contour.ts
@@ -1,44 +1,54 @@
-import { BoxGeometry, ShaderMaterial, Vector2, Vector3, Mesh, Scene, WebGLRenderer, OrthographicCamera } from "three";
+import {
+  BoxGeometry,
+  ShaderMaterial,
+  Vector2,
+  Vector3,
+  Mesh,
+  Scene,
+  WebGLRenderer,
+  OrthographicCamera,
+} from "three";
 import Color from "colorjs.io";
-import contourFrag from "../shaders/contour.frag?raw"
-import contourVert from "../shaders/contour.vert?raw"
-import { canvasId } from "./contourConsts"
+import contourFrag from "../shaders/contour.frag?raw";
+import contourVert from "../shaders/contour.vert?raw";
+import { canvasId } from "./contourConsts";
 
-const darkModeEnabled = () => document.documentElement.classList.contains("dark");
+const darkModeEnabled = () =>
+  document.documentElement.classList.contains("dark");
 const isDarkInit = darkModeEnabled();
 
 const getColorVector = (isDark: boolean) => {
-    let styles = getComputedStyle(document.documentElement);
-    let styleName = isDark ? "--color-dark-background" : "--color-background";
-    let style = styles.getPropertyValue(styleName);
-    let color = new Color(style).to('srgb');
-    return new Vector3(color.r * 255, color.g * 255, color.b * 255);
-}
+  let styles = getComputedStyle(document.documentElement);
+  let styleName = isDark ? "--color-dark-background" : "--color-background";
+  let style = styles.getPropertyValue(styleName);
+  let color = new Color(style).to("srgb");
+  return new Vector3(color.r * 255, color.g * 255, color.b * 255);
+};
 
 const geometry = new BoxGeometry(100, 100);
 const material = new ShaderMaterial({
-    fragmentShader: contourFrag,
-    vertexShader: contourVert,
-    uniforms: {
-        iTime: { value: 20 },
-        iResolution: {
-            value: new Vector2(window.innerWidth, window.innerHeight),
-        },
-        bgColor: {
-            value: getColorVector(isDarkInit)
-        },
-        colorMult: {
-            value: isDarkInit ? 1.0 : -1.0
-        }
+  fragmentShader: contourFrag,
+  vertexShader: contourVert,
+  uniforms: {
+    iTime: { value: 20 },
+    iResolution: {
+      value: new Vector2(window.innerWidth, window.innerHeight),
     },
+    bgColor: {
+      value: getColorVector(isDarkInit),
+    },
+    colorMult: {
+      value: isDarkInit ? 1.0 : -1.0,
+    },
+  },
 });
 const mesh = new Mesh(geometry, material);
 const scene = new Scene();
 scene.add(mesh);
 const canvas = document.getElementById(canvasId)!;
 const renderer = new WebGLRenderer({
-    canvas: canvas,
-    antialias: true,
+  canvas: canvas,
+  antialias: true,
 });
 const camera = new OrthographicCamera();
 let lastRender: DOMHighResTimeStamp | undefined = undefined;
@@ -48,44 +58,41 @@ let innerHeight = window.innerHeight;
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-
 let wasDark = darkModeEnabled();
 
 const animate = (timestamp: DOMHighResTimeStamp) => {
-    if (window.devicePixelRatio != devicePixelRatio) {
-        renderer.setPixelRatio(window.devicePixelRatio);
-        devicePixelRatio = window.devicePixelRatio;
+  if (window.devicePixelRatio != devicePixelRatio) {
+    renderer.setPixelRatio(window.devicePixelRatio);
+    devicePixelRatio = window.devicePixelRatio;
+  }
+  if (window.innerWidth != innerWidth || window.innerHeight != innerHeight) {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    innerWidth = window.innerWidth;
+    innerHeight = window.innerHeight;
+  }
+
+  material.uniforms["iResolution"].value.x = window.innerWidth;
+  material.uniforms["iResolution"].value.y = window.innerHeight;
+
+  const isDark = darkModeEnabled();
+
+  if (isDark !== wasDark) {
+    material.uniforms["bgColor"].value = getColorVector(isDark);
+    if (isDark) {
+      material.uniforms["colorMult"].value = 1.0;
+    } else {
+      material.uniforms["colorMult"].value = -1.0;
     }
-    if (window.innerWidth != innerWidth || window.innerHeight != innerHeight) {
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        innerWidth = window.innerWidth;
-        innerHeight = window.innerHeight;
-    }
+    wasDark = isDark;
+  }
 
-    material.uniforms["iResolution"].value.x = window.innerWidth;
-    material.uniforms["iResolution"].value.y = window.innerHeight;
+  if (lastRender && material.uniforms["iTime"].value <= 100) {
+    const delta = timestamp - lastRender;
+    material.uniforms["iTime"].value += delta / 500;
+  }
+  lastRender = timestamp;
 
-    const isDark = darkModeEnabled();
-
-    if (isDark !== wasDark) {
-        material.uniforms["bgColor"].value = getColorVector(isDark);
-        if (isDark) {
-            material.uniforms["colorMult"].value = 1.0;
-        } else {
-            material.uniforms["colorMult"].value = -1.0;
-        }
-        wasDark = isDark;
-    }
-
-
-    if (lastRender && material.uniforms["iTime"].value <= 100) {
-        const delta = timestamp - lastRender;
-        material.uniforms["iTime"].value += delta / 500;
-    }
-    lastRender = timestamp;
-
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 };
 
 renderer.setAnimationLoop(animate);
-
